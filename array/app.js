102a0e8bf610d1ec5e179604a9a95fbb5673c47f
Chauvin <chauvin-e@orange.fr>
2017-03-27 11:27:57 +0200

    Exercice sur Javascript

--- array/app.js
+++ array/app.js
@@ -1,4 +1,17 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 
-var tab = array.push("courgette");
+// Exercice 8
+array.push("courgette");
+console.log(array);
+
+// Exercice 9
+array.splice(4, 0, "Citron");
+console.log(array);
+
+// Exercice 10
+array.splice(1, 1);
+console.log(array);
+
+// Exercice 11
+array.splice(2, 0, "Poire");
 console.log(array);
