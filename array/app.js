aba2a9cca510211ee39e9dd12e16af96bf11c4e9
Chauvin <chauvin-e@orange.fr>
2017-03-27 16:59:13 +0200

    Corrections des exos sur les tableaux avec Javascript.

--- array/app.js
+++ array/app.js
@@ -4,9 +4,13 @@ var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 array.push("Courgette");
 console.log(array);
 
-// Exercice 9
+/* Exercice 9
 array.splice(4, 0, "Citron");
-console.log(array);
+console.log(array);*/
+
+// Correction exo 9
+array[4].push("Citron");
+console.log("Citron");
 
 // Exercice 10
 array.splice(1, 1);
@@ -15,16 +19,3 @@ console.log(array);
 // Exercice 11
 array.splice(2, 0, "Poire");
 console.log(array);
-
-/* Exercice 12
-for (i = 0; i < array.lenght; i++) {
-	console.log(array[[i]]);
-};*/
-
-// Tentative de correction avec Jordan
-for (var i = 0, i < array.lenght, i++) {
-	console.log(array[i]);
-	for (var l = 0, l < array[i].lenght, i++) {
-		console.log(array[i][l]);
-	}
-};
