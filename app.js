b4f622272c0a5e200369c011c7bedec9fb9af716
Chauvin <chauvin-e@orange.fr>
2017-03-27 15:11:07 +0200

    Je continue les exos sur les fonctions...

--- app.js
+++ app.js
@@ -46,13 +46,42 @@ console.log(result);
 function chaine(string) {
 	return string;
 }
-
+var mot = chaine("Hello");
+console.log(mot);
 
 
 // Exercice 3
 function foo(string, chaine) {
-	return string + chaine;
+	return string + " " + chaine;
 }
-console.log(foo); 
+var afficher = foo("Hello", "World");
+console.log(afficher);
+
 
 // Exercice 4
+function nombre(a, b) {
+	if(a > b) {
+		return "Le premier nombre est plus grand!";
+	} else if (a < b) {
+		return "Le premier nombre est plus petit!";
+	} else {
+		return "Les deux nombres sont identiques!";
+	}
+}
+var resultat = nombre(78, 34);
+console.log(resultat);
+
+// Exercice 5
+function conca(c, mot) {
+	return c + " " + mot;
+}
+var phrase = conca(2, "hellos");
+console.log(phrase);
+
+
+// Exercice 6
+function bienvenue(nom, prenom, age) {
+	return "Bonjour" + nom + prenom + ",tu as" + age + "ans.";
+}
+var identité = ("Dupont", "Jean", 32);
+console.log(identité);
