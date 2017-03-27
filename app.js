30f79c735da06405475e3028582cb34ca0eaa8e8
Chauvin <chauvin-e@orange.fr>
2017-03-27 10:55:54 +0200

    Je continu l'exerice

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Exercice 1
-mois = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre","octobre", "novembre", "décembre"];
+var mois = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre","octobre", "novembre", "décembre"];
 console.log(mois);
 
 //Exercice 2
@@ -9,4 +9,8 @@ console.log(mois[3]);
 console.log(mois[5]);
 
 // Exercice 4
-console.log(mois)
+console.log(mois[10]);
+
+// Exercice 5
+ console.log(mois.splice(7, 1));
+ console.log(mois.splice(7, 0, "août"));
