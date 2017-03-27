7645f7c0daa95156217bd94fc6df78b071cd6d39
Chauvin <chauvin-e@orange.fr>
2017-03-27 10:39:14 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// Exercice 1
+mois = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre","octobre", "novembre", "décembre"];
+console.log(mois);
+
+//Exercice 2
+console.log(mois[3]);
+
+// Exercice 3
+console.log(mois[5]);
+
+// Exercice 4
+console.log(mois)
